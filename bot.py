943f399ffc414e8d1867b530522251f35fac6157
Laith Taher <86690869+LOTaher@users.noreply.github.com>
2022-12-28 10:38:14 -0800

    fixed spacing and added comments

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 from typing import Literal
 import json
 
-
 class Client(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned, intents = discord.Intents.default())
@@ -18,7 +17,7 @@ class Client(commands.Bot):
     async def on_ready(self):
         print(f"Logged in as {self.user}.")
         await self.tree.sync()
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Church Customs'))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='Church Customs')) # Custom Presence. 
 
 with open("config.json", "r") as f:
     data = json.load(f)
